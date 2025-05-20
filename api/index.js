7230ad4cc6993dc9f897db938522fc66bf4bc861
Abishalini1602 <abiabishalini1002@gmail.com>
2025-05-20 09:11:47 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,7 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(() => {
+    console.log("Connected to MongoDb!");
+})
+ .catch((err) =>{
+    console.log(err);
+
+})
 
 const app = express();
+
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!")
 })
+
+// uqpfbxxGcbZhZx7M
+// abishalini353    ddbgzaxwd2au3K4G
